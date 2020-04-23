767d9eeec29c83d427d15c14116dc9b85e3a4aa4
Menna Tarek <mennatareeq@gmail.com>
2020-04-23 21:12:46 +0200

    initial commit

---
+++ URLDownload.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 21:13:52 2018
+
+@author: Menna
+"""
+
+
+import urllib 
+import datetime
+
+infile = open("Path.txt", 'r')
+firstLine = infile.readline()
+
+now = datetime.datetime.now()
+date = now.strftime("%Y-%m-%d")
+
+url = 'https://us.spindices.com/documents/additional-material/sp-500-eps-est.xlsx'  
+urllib.urlretrieve(url, firstLine + '/sp-500-eps-est-' + date + '.xlsx') 
+
+print(firstLine)
+input("press close to exit") 
+
